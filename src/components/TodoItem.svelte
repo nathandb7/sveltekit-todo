9a7e509d806d37db1d2dd3f166a0d5068610467e
nathandb7 <nathandb7@gmail.com>
2024-09-26 11:18:45 -0300

    todo svelte & firebase

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let index;
+    export let todo;
+    export let editTodo;
+    export let removeTodo;
+</script>
+
+<div class="todo">
+    <p>
+        {index + 1}. {todo}
+    </p>
+    <div class="actions">
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <i
+            on:click={() => {
+                editTodo(index);
+            }}
+            on:keydown={() => {}}
+            class="fa-solid fa-pen-to-square"
+        ></i>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <i
+            on:click={() => {
+                removeTodo(index);
+            }}
+            on:keydown={() => {}}
+            class="fa-solid fa-trash-can"
+        ></i>
+    </div>
+</div>
+
+<style>
+   .todo {
+        border-left: 1px solid cyan;
+        padding: 8px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .actions {
+        display: flex;
+        align-items: center;
+        gap: 14px;
+        font-size: 1.3rem;
+    }
+
+    .actions i {
+        cursor: pointer;
+    }
+
+    .actions i:hover {
+        color: coral;
+    }
+</style>
